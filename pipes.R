8a84500f6999b5a3c7c89b01ad22ce3118ef421e
rravitteja <rravitteja@gmail.com>
2017-11-15 16:06:05 +0530

    initial commit

--- pipes.R
+++ pipes.R
@@ -1,5 +1,39 @@
-data<-c(1,3,5,7,11,17)
+library("tidyverse")
+8+9
+data <-c(1,3,5,7,11,13)
 
-data
+diff(data) # difference between numbers
 
-diff(data)
+#pipe
+
+data %>%
+  diff()
+
+#mean with differences
+data %>%
+  diff() %>%
+  mean()
+
+#mean 
+data %>%
+  mean()
+
+
+mean(diff(data))
+
+#square 
+
+data^2
+
+diff(data^2)
+
+data^2 %>%
+  diff()
+
+data^2 %>%
+  diff(lag=2)
+
+
+data^2 %>%
+  diff(lag=2) %>%
+  mean()
